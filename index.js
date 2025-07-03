da6626f7d77e95586957d37fb532bf3bcfa15586
Adam Reynolds <github@smufflersworld.com>
2025-07-03 16:35:32 +0100

    Get the basics working

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const { v4: uuidv4 } = require('uuid');
 require('dotenv').config();
 const axios = require('axios');
 
-setInterval(fetchPrivateMessages, 5000); // Poll every 5 seconds
-
 const {
   COMETCHAT_API_KEY,
   COMETCHAT_APP_ID,
@@ -15,7 +13,7 @@ const {
   CHAT_COLOUR
 } = process.env;
 
-const BASE_URL = `https://${COMETCHAT_API_KEY}.apiclient-us.cometchat.io/v3`;
+const BASE_URL = `https://${COMETCHAT_API_KEY}.apiclient-us.cometchat.io`;
 
 const headers = {
   'Content-Type': 'application/json',
@@ -50,7 +48,7 @@ async function sendPrivateMessage() {
     }
   };
 
-  const response = await axios.post(`${BASE_URL}/messages`, payload, { headers });
+  const response = await axios.post(`${BASE_URL}/v3.0/messages`, payload, { headers });
   console.log('✅ Private message sent:', JSON.stringify(response.data,  null, 2) );
 }
 
@@ -82,21 +80,69 @@ async function sendGroupMessage() {
     }
   };
 
-  const response = await axios.post(`${BASE_URL}/messages`, payload, { headers });
+  const response = await axios.post(`${BASE_URL}/v3.0/messages`, payload, { headers });
   console.log('✅ Group message sent:', JSON.stringify(response.data,  null, 2) );
 }
 
 async function fetchPrivateMessages() {
-  const response = await axios.get(`${BASE_URL}/messages?receiver=${COMETCHAT_UID}&receiverType=user`, {
-    headers
-  });
 
-  console.log('📥 Private messages:', response.data.data.map(msg => `${msg.sender}: ${msg.data.text}`));
+  const paths = [
+    'v3',
+    'messages'
+  ]
+  const searchParams = [
+    [ 'receiverType', 'user' ],
+    [ 'category', 'message' ],
+    [ 'limit', 10 ],
+    [ 'receiver', COMETCHAT_UID ]
+  ]
+
+  const pmHeaders = {
+    accept: 'application/json',
+    apikey: COMETCHAT_APP_ID
+  };
+
+
+  const PM_URL = `https://${COMETCHAT_API_KEY}.api-us.cometchat.io`;
+  const url = buildUrl( PM_URL, paths, searchParams )
+  console.log(pmHeaders);
+  console.log( await url);
+  const response = await axios.get( await url, {
+    pmHeaders
+  });
+  // console.log('📥 Private messages:', response.data.data.map(msg => `${msg.sender}: ${msg.data.text}`));
+  console.log('📥 Private messages:', JSON.stringify(response.data,  null, 2) );
 }
 
+async function buildUrl ( host, paths = [], searchParams, protocol = 'https' ) {
+  const url = new URL( paths.join( '/' ), `${ host }` )
+  url.search = new URLSearchParams( searchParams )
+  return url
+}
 
 async function fetchGroupMessages() {
-  const response = await axios.get(`${BASE_URL}/messages?receiver=${COMETCHAT_GROUP_ID}&receiverType=group`, {
+  const messageLimit = 50
+  const paths = [
+    'v3.0',
+    'groups',
+    COMETCHAT_GROUP_ID,
+    'messages'
+  ]
+  const searchParams = [
+    [ 'per_page', messageLimit ],
+    [ 'hideMessagesFromBlockedUsers', 0 ],
+    [ 'unread', 0 ],
+    [ 'undelivered', 1 ],
+    [ 'withTags', 0 ],
+    [ 'hideDeleted', 0 ],
+    // [ 'sentAt', fromTimestamp ],
+    [ 'affix', 'append' ],
+    [ 'id', 25323881 ]
+  ]
+
+  const url = buildUrl( BASE_URL, paths, searchParams )
+  console.log( await url);
+  const response = await axios.get( await url, {
     headers
   });
   console.log('📥 Group messages:', response.data.data.map(msg => `${msg.sender}: ${msg.data.text}`));
@@ -104,10 +150,17 @@ async function fetchGroupMessages() {
 
 (async () => {
   try {
-    await sendPrivateMessage();
-    await sendGroupMessage();
-    await fetchGroupMessages();
-  } catch (error) {
+    // await sendPrivateMessage();
+    // await sendGroupMessage();
+    // setInterval(() => {
+    //   fetchGroupMessages().catch(console.error);
+    // }, 5000);
+
+    setInterval(() => {
+      fetchPrivateMessages().catch(console.error);
+    }, 5000);  
+  } 
+  catch (error) {
     console.error('❌ Error:', error.response?.data || error.message);
   }
 })();
