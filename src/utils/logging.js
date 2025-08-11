b8a8fd92b10c34eab4d125a984a851764ed38bc9
Adam Reynolds <github@smufflersworld.com>
2025-08-11 15:24:50 +0100

    Added better logging, and updated everything to use it!

--- src/utils/logging.js
+++ src/utils/logging.js
@@ -25,13 +25,6 @@ const logger = winston.createLogger({
         winston.format.simple()
       )
     }),
-    new winston.transports.File({ 
-      filename: 'logs/error.log', 
-      level: 'error' 
-    }),
-    new winston.transports.File({ 
-      filename: 'logs/combined.log' 
-    }),
     new winston.transports.DailyRotateFile({
       dirname: logsDir,
       filename: '%DATE%.log',
