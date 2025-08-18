ce457576c13b6bdf16eeface01ba0ecc51fd17c7
Adam Reynolds <github@smufflersworld.com>
2025-08-18 10:53:36 +0100

    WiP: temp test time

---
+++ check-dotenv.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+console.log('Dotenv loaded successfully');
+console.log('Environment variables:');
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('BOT_UID:', process.env.BOT_UID);
+console.log('CHAT_AVATAR_ID:', process.env.CHAT_AVATAR_ID);
+console.log('CHAT_NAME:', process.env.CHAT_NAME);
+console.log('CHAT_COLOUR:', process.env.CHAT_COLOUR);
+console.log('COMMAND_SWITCH:', process.env.COMMAND_SWITCH);
